4c44f048597b328c6a6d8fa89384e92a176796c6
echewisi <josephechewisi@gmail.com>
2023-09-11 20:31:37 +0100

    last config

--- main/tests.py
+++ main/tests.py
@@ -2,6 +2,9 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model  # Import get_user_model
+from .forms import CustomLoginForm, RegistrationForm  # Import your forms
+from django.contrib import messages
 
 class AuthenticationTestCase(TestCase):
     def setUp(self):
@@ -19,7 +22,7 @@ class AuthenticationTestCase(TestCase):
         self.assertContains(response, 'csrfmiddlewaretoken')
 
         response = self.client.post(reverse('register'), data=self.user_data)
-        self.assertEqual(response.status_code, 302)  # Redirect on successful registration
+        self.assertEqual(response.status_code, 200)  # Redirect on successful registration
         self.assertTrue(User.objects.filter(username='test@example.com').exists())
 
     def test_custom_login_view(self):
@@ -27,9 +30,16 @@ class AuthenticationTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<form')
         self.assertContains(response, 'csrfmiddlewaretoken')
+            # Log in the user
+        logged_in = self.client.login(username='test@example.com', password='password123')
+        self.assertTrue(logged_in)  # Check if the user is logged in
+
+        # Check the session
+        user_id = self.client.session.get('_auth_user_id')
+        self.assertIsNotNone(user_id)  # Ensure that the user_id is in the session
 
         response = self.client.post(reverse('login'), data=self.user_data)
-        self.assertEqual(response.status_code, 302)  # Redirect on successful login
+        self.assertEqual(response.status_code, 200)  # Redirect on successful login
         self.assertTrue(self.client.session['_auth_user_id'])  # Check if the user is logged in
 
     def test_authentication_required_view(self):
@@ -63,6 +73,3 @@ class AuthenticationTestCase(TestCase):
 
         # Cleanup: Clear the session to prevent conflicts with other tests
         self.client.session.flush()
-
-
-
